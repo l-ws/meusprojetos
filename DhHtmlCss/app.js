ab1a11500752517b9dff0f67d6c67d9a078ab451
l-ws <contlucas@outlook.com>
2022-01-28 20:04:11 -0300

    Meu  primeiro servidor salvo

--- DhHtmlCss/app.js
+++ DhHtmlCss/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get("/num",(req, res)=>
-    res.send("Voce esta na numgit "));
+    res.send("Voce esta na num "));
 app.listen(8000,()=>{
     console.log("Levantando um servidor com Express")    
 })
